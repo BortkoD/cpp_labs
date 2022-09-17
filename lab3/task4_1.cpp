3dbf2d917f2b75e8f456de9e0a0dca4dea981030
BortkoD <megymikei@gmail.com>
2022-09-17 14:30:07 +0300

    feat: add task 4 part 1 for lab 3

--- lab3/task4_1.cpp
+++ lab3/task4_1.cpp
@@ -1,14 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int addNumders(int n)
+int addNumbers(int n)
 {
     if (n == 1) return 1; // выход из рекурсии
-    else return (n + addNumders(n - 1));
+    else return (n + addNumbers(n - 1));
+}
+
+int addNumbers (int from, int to) {
+    if (from == to) return from;
+    else return (from + addNumbers(from + 1, to));
 }
 
 
 int main() {
+
+    int n;
+    cout << "Please enter the number: ";
+    cin >> n;
+    cout << "Sum from 1 to " << n << " = " << addNumbers(n) << endl;
+
+    int a, b;
+    cout << "Please enter two numbers: ";
+    cin >> a >> b;
+    if (a > b) {
+        int temp = a;
+        a = b; b = temp;
+    }
+    cout << "Sum from " << a << " to " << b << " = " << addNumbers(a, b) << endl;
 
 
     return 0;
