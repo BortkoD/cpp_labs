eceb67856b8b2f3bc9c73f45ec3c1cfa26410ebc
BortkoD <megymikei@gmail.com>
2022-09-18 12:44:19 +0300

    feat: add control task 4 solution

--- lab3/control4.cpp
+++ lab3/control4.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int summarize (int n) {
+    if (n == 0) return 0; 
+    return (5*n + summarize(n-1));
+}
 
+int main() {
+    int n;
+    cout << "Please enter N value: ";
+    cin >> n;
+    
+    int S = summarize(n);
+
+    cout << "The sum is " << S << endl;
 
     return 0;
 }
