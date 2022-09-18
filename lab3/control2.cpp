f1dd5aec40470043afa9c9b6bd195670704f1d6f
BortkoD <megymikei@gmail.com>
2022-09-18 11:55:57 +0300

    feat: add solution for control task 2

--- lab3/control2.cpp
+++ lab3/control2.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
-int main() {
+double sqrt3 (int a) {
+    return pow(a, 1.0/3);
+}
 
+double iter_sqrt (double a, double x, double x1) {
+    while (true) {
+        x1 = (1.0 / 3) * (a / (x * x) + 2 * x);
+
+        if (abs(x - x1) < 0.001) break;
+
+        x = x1;
+    }
+    return x;
+}
+
+int main() {
+    double a;
+    cout << "Please, enter the number: ";
+    cin >> a;
+
+    cout << "With the built in function we get "<< sqrt3(a) << endl;
+    cout << "With the iterative formula, we get " << iter_sqrt (a, a, 0) << endl;
 
     return 0;
 }
