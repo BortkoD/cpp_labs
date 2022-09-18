a8721d35fec4cd5dc2c689b12cd9ae7ecff7b1d0
BortkoD <megymikei@gmail.com>
2022-09-18 13:04:05 +0300

    feat: add solution for control 5

--- lab3/control5.cpp
+++ lab3/control5.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+string binary (int num) {
+    if (num == 0) return "";
 
+    string s = "0";
+    if (num % 2 == 1) 
+        s = "1";
+
+    return binary( num / 2 ) + s;
+}
+
+int main() {
+    int n;
+    cout << "Please enter a number in decimal notation: ";
+    cin >> n;
+
+    cout << "Your number in binary notation is " << binary(n) << endl;
 
     return 0;
 }
