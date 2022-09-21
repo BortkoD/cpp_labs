60fb1cb241c52c47892377d1ef9fbd0597283b50
BortkoD <megymikei@gmail.com>
2022-09-21 18:20:07 +0300

    feat: add solution for control 3 task

--- lab5/control3.cpp
+++ lab5/control3.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int search (int arr[], int n, int key) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key && i != 0) {
+            swap(arr[i], arr[i-1]);
+            return (i-1); 
+        } 
+    }
+    return 0;
+}
 
+int main() {
+    int n;
+    cout << "Enter array size: ";
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++ ) {
+        cout << "arr[" << i << "] = ";
+        cin >> arr[i];
+    }
+
+    int key;
+    cout << "Enter key value: ";
+    cin >> key;
+    cout << "Current key position is " << search(arr, n, key) << endl;
+
+    for (int i = 0; i < n; i++ ) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 
     return 0;
 }
